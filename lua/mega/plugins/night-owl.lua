a64e5fb1a3965670cd2e8bfcca5c5bc40528d189
Mouaaz Sweid <mmsweid@gmail.com>
2025-06-05 16:04:45 +0300

    add transparency to my theme

--- lua/mega/plugins/night-owl.lua
+++ lua/mega/plugins/night-owl.lua
@@ -6,5 +6,33 @@ return {
     -- load the colorscheme here
     require('night-owl').setup()
     vim.cmd.colorscheme 'night-owl'
+
+    -- Set transparency
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'FoldColumn', { bg = 'none' })
+
+    -- Hide EndOfBuffer characters
+    vim.opt.fillchars:append { eob = ' ' } -- Remove ~ lines
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+
+
+    -- For common plugins
+    vim.api.nvim_set_hl(0, 'NvimTreeNormal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+
+    -- Bottom command line styling
+    vim.opt.cmdheight = 0 -- Height of command line
+    vim.api.nvim_set_hl(0, 'StatusLine', {
+      bg = 'none',
+      fg = '#7aa2f7',
+      bold = true,
+    })
+
+    -- Hide bottom statusline when not needed
+    vim.opt.laststatus = 0 -- Global statusline
+    vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none' })
   end,
 }
